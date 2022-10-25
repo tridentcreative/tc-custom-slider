f9816a5ec493fe1838d823926a3ea99c9c84c497
tridentcreative <62360082+tridentcreative@users.noreply.github.com>
2022-10-25 12:32:53 +1000

    Update custom-slider.js

--- custom-slider.js
+++ custom-slider.js
@@ -1,4 +1,4 @@
-var current_slide_1, end_slide_1, end_slide_2, change_slide_status_1, change_slide_status_2, get_image_1, get_image_2, set_delay_1, set_delay_2, current_slide_2, get_video_id_1, get_video_id_2, set_fade_1, set_fade_2, full_video_link_1, full_video_link_2, active_element_1, active_element_2, get_header_1, get_header_2, get_sub_header_1, get_sub_header_2, get_summary_1, get_summary_2;
+var current_slide_1, current_slide_2, end_slide_1, end_slide_2, change_slide_status_1, change_slide_status_2, get_image_1, get_image_2, set_delay_1, set_delay_2, get_video_id_1, get_video_id_2, set_fade_1, set_fade_2, full_video_link_1, full_video_link_2, active_element_1, active_element_2, get_header_1, get_header_2, get_sub_header_1, get_sub_header_2, get_summary_1, get_summary_2;
 
 // Describe this function...
 function change_current_slide_1() {
@@ -54,7 +54,7 @@ function set_content_2() {
   $("[tc-slider-content-2='set-video']").attr('src',full_video_link_2);
   $("[tc-slider-content-2='set-image']").fadeOut( set_fade_2);
   setTimeout(function(){
-          $("[tc-slider-content-2='set-image']").attr( "src",get_image_2);
+          $("[tc-slider-content-2='set-image']").attr( 'src',get_image_2);
     setTimeout(function(){
             $("[tc-slider-content-2='set-image']").fadeIn( set_fade_2);
      }, 50);
@@ -118,27 +118,27 @@ set_fade_1 = $("[tc-slider-content-1='get-fade']").text();
 change_slide_status_1 = true;
 get_content_1();
 slider_action_1();
-if (set_delay_1 <= 0) {
+if ($.isNumeric(set_delay_1) == false) {
   change_slide_status_1 = false;
 }
-if (set_fade_1 <= 0) {
+if ($.isNumeric(set_fade_1) == false) {
   set_fade_1 = 350;
 }
 set_delay_1 = parseFloat(set_delay_1);
 set_fade_1 = parseFloat(set_fade_1);
 
-current_slide_1 = 1;
+current_slide_2 = 1;
 end_slide_2 = $("[tc-slider-cms-2='item']").length;
 set_delay_2 = $("[tc-slider-content-2='get-delay']").text();
 set_fade_2 = $("[tc-slider-content-2='get-fade']").text();
 change_slide_status_2 = true;
 get_content_2();
 slider_action_2();
-if (set_fade_2 <= 0) {
-  set_fade_2 = 350;
-}
-if (set_delay_2 <= 0) {
+if ($.isNumeric(set_delay_2) == false) {
   change_slide_status_2 = false;
+}
+if ($.isNumeric(set_fade_2) == false) {
+  set_fade_2 = 350;
 }
 set_delay_2 = parseFloat(set_delay_2);
 set_fade_2 = parseFloat(set_fade_2);
@@ -148,13 +148,13 @@ $("[tc-slider-cms-1='item']").on('click',function() {
   current_slide_1 = $(this).index() + 1;
   get_content_1();
   set_content_1();
-  setTimeout(function(){
-          change_current_slide_1();
-    if (set_delay_1 > 0) {
+  if ($.isNumeric(set_delay_1) == true) {
+    setTimeout(function(){
+            change_current_slide_1();
       change_slide_status_1 = true;
       slider_action_1();
-    }
-   }, set_delay_1);
+     }, set_delay_1);
+  }
 });
 
 $("[tc-slider-cms-2='item']").on('click',function() {
@@ -162,25 +162,25 @@ $("[tc-slider-cms-2='item']").on('click',function() {
   current_slide_2 = $(this).index() + 1;
   get_content_2();
   set_content_2();
-  setTimeout(function(){
-          change_current_slide_2();
-    if (set_delay_2 > 0) {
+  if ($.isNumeric(set_delay_2) == true) {
+    setTimeout(function(){
+            change_current_slide_2();
       change_slide_status_2 = true;
       slider_action_2();
-    }
-   }, set_delay_2);
+     }, set_delay_2);
+  }
 });
 
 $("[tc-slider-cms-1='item']").on('mouseover',function() {
   $(this).find("[tc-slider-content-1='interactive-element']").addClass('hover-element-1');
 });
 
-$("[tc-slider-cms-1='item']").on('mouseout',function() {
-  $(this).find("[tc-slider-content-1='interactive-element']").removeClass('hover-element-1');
-});
-
 $("[tc-slider-cms-2='item']").on('mouseover',function() {
   $(this).find("[tc-slider-content-2='interactive-element']").addClass('hover-element-2');
+});
+
+$("[tc-slider-cms-1='item']").on('mouseout',function() {
+  $(this).find("[tc-slider-content-1='interactive-element']").removeClass('hover-element-1');
 });
 
 $("[tc-slider-cms-2='item']").on('mouseout',function() {
